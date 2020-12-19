f384fdb4152677ca4077cabfdf20250bbad7eb31
RadhikaRJ <radhika7002@gmail.com>
2020-12-19 12:24:40 +0530

    feat: leapyear

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
-var inputref = document.querySelector("#input");
+var txtref = document.querySelector("#txt-input");
+var outputref = document.querySelector("#output");
 
-var btnref =document.querySelector("#btn");
+var btnref = document.querySelector("#btn");
+btnref.addEventListener("click", clickHandler);
 
-var outputref =document.querySelector("#result");
+function clickHandler() {
+    var inputText = txtref.value;
+    var formatCheck = checkInputFormat(inputText);
 
-console.log ("you are here");
+    if (formatCheck == true) {
+        
+        var leap = isLeapYear(inputText);
+        if (leap == true) {
+            outputref.innerText = "Yes! You are born in a leap year!\n Feel free to share this with your friend circle over social media."
+        } else {
+            outputref.innerText = "You were not born in a leap year!"
+        }
+    } else {
+        alert("You input is invalid or is not in the required format!\n Required format: DD-MM-YYYY");
+    }
 
-btnref.addEventListener("click",clickHandler);
+}
 
-function clickHandler(){
-    console.log ("button is clicked");
-    var inputvalue= inputref.value;
-    outputref.innerText=inputvalue;
+function checkInputFormat(ip) {
+    var [daycheck, monthcheck, yearcheck] = ip.toString().split("-")
+
+    if (monthcheck === null || monthcheck == " " || yearcheck == " " || yearcheck === null || isNaN(daycheck) ==true|| isNaN(monthcheck)==true||isNaN(yearcheck)) {
+        return false;
+    } else {
+
+        return true;
+    }
+}
+
+function isLeapYear(ip) {
+    var [day, month, year] = ip.toString().split("-")
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+
+        return true;
+    } else {
+
+        return false;
+    }
 }
